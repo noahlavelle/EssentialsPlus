b6bba98dbc05aff194abdb614de031f5cc5ca07c
BuildTools <noah.lavelle@gmail.com>
2021-01-09 13:39:19 +0000

    begun bedwars

--- src/com/noahlavelle/essentialsplus/games/BedWars.java
+++ src/com/noahlavelle/essentialsplus/games/BedWars.java
@@ -8,11 +8,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class BedWars implements CommandExecutor {
 
     private Main plugin;
+
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    Map<String, Game> pendingGames = new HashMap<>();
 
     public BedWars (Main plugin) {
         this.plugin = plugin;
@@ -25,14 +32,65 @@ public class BedWars implements CommandExecutor {
 
         Player player = ((Player) commandSender).getPlayer();
 
-        World source = Bukkit.getWorld("bw-speedway");
-
-        WorldManager.copyWorld(source, "bw-" + player.getUniqueId().toString());
-
-        Location mapTP = new Location(Bukkit.getWorld("bw-" + player.getUniqueId().toString()), 0, 100, 0);
-
-        player.teleport(mapTP);
+        if (pendingGames.isEmpty()) {
+            createGame(player);
+        } else {
+            joinGame(player, pendingGames.get(pendingGames.keySet().toArray()[0]));
+        }
 
         return false;
+    }
+
+    public void createGame (Player player) {
+        Random random = new Random(System.currentTimeMillis());
+        String id = generateGameID(random);
+
+        Game game = new Game (id);
+
+        pendingGames.put(id, game);
+
+        World source = Bukkit.getWorld("bw-speedway");
+        WorldManager.copyWorld(source, "bw-" + id);
+
+       ;
+
+        String[] spawnCoords =  plugin.getConfig().getString("bedwars.bw-speedway.spawnPoints.wait");
+
+        Location mapTP = new Location(Bukkit.getWorld("bw-" + id), 0, 100, 0);
+        player.teleport(mapTP);
+    }
+
+    public String generateGameID (Random random) {
+        String id = "";
+
+        for (int i = 0; i <= 5; i++) {
+            id += alphabet.charAt(random.nextInt(alphabet.length()));
+        }
+
+        if (pendingGames.containsKey(id)) {
+            generateGameID(random);
+            return "";
+        } else {
+            return id;
+        }
+
+    }
+
+    public void joinGame (Player player, Game game) {
+        System.out.println(game.id);
+        Location mapTP = new Location(Bukkit.getWorld("bw-" + game.id), 0, 100, 0);
+        player.teleport(mapTP);
+    }
+
+    public static class Game {
+
+        String id;
+
+        public Game (String id) {
+            this.id = id;
+        }
+
+        ArrayList<Player> players = new ArrayList<>();
+
     }
 }
